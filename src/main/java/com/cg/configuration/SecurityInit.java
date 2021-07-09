37ee03f68a38adbf5339ace5a80f69ef99c6d534
Nguyễn Tâm Thiện <79630378+thiennguyen161093@users.noreply.github.com>
2021-07-09 15:54:13 +0700

    update

---
+++ src/main/java/com/cg/configuration/SecurityInit.java
@@ -0,0 +1,8 @@
+package com.cg.configuration;
+
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
+
---
+++ src/main/java/com/cg/service/user/IUserService.java
@@ -0,0 +1,11 @@
+package com.cg.service.user;
+
+import com.cg.model.User;
+import com.cg.service.IGeneralService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.Optional;
+
+public interface IUserService extends IGeneralService<User>, UserDetailsService {
+    Optional<User> findByUsername(String username);
+}
